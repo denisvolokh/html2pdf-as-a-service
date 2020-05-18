d84b533cfacc1fe8c04446ca490ea57f6bfb200b
Denis Volokh <denis.volokh@gmail.com>
2020-05-18 15:36:21 +0800

    changed to body parameters

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # html2pdf-as-a-service
 
-curl --request POST \
-  --url 'http://0.0.0.0:3000/html2pdf?url=http://www.google.com'
+curl -d '{"url":"www.google.com"}' -H "Content-Type: application/json" -X POST http://0.0.0.0:3000/html2pdf
--- index.js
+++ index.js
@@ -5,25 +5,48 @@ const asyncHandler = require('express-async-handler')
 const app = require('express')()
 const port = 3000
 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.post('/data', function(req, resp) {
+    console.log("[+] Request Body", req.body);
+
+    resp.end();
+})
+
 app.post('/html2pdf', asyncHandler(async (req, resp, next) => {
-    console.log(req.query)
-    if (req.query == undefined) {
-        resp.status(400).send("The query parameters are missing.")
+    console.log("[+] Request Body", req.body.url);
 
-    } else if (!("url" in req.query) || req.query.url == "") {
-        resp.status(400).send("The url parameter is missing.")
-
-    } else {
+    if (!("url" in req.body) || req.body.url == "") {
         try {
-            const pdf_path_filename = await actions.html2pdf(req.query)
+            const pdf_path_filename = await actions.html2pdf(req.body)
             resp.setHeader('Content-disposition', 'attachment; filename=' + pdf_path_filename)
             resp.download(pdf_path_filename)
 
         } catch (e) {
             resp.status(400).send('Failed to convert to PDF file: ' + e.message)
         }
-        
     }
+
+    // if (req.query == undefined) {
+    //     resp.status(400).send("The query parameters are missing.")
+
+    // } else if (!("url" in req.query) || req.query.url == "") {
+    //     resp.status(400).send("The url parameter is missing.")
+
+    // } else {
+    //     try {
+    //         const pdf_path_filename = await actions.html2pdf(req.query)
+    //         resp.setHeader('Content-disposition', 'attachment; filename=' + pdf_path_filename)
+    //         resp.download(pdf_path_filename)
+
+    //     } catch (e) {
+    //         resp.status(400).send('Failed to convert to PDF file: ' + e.message)
+    //     }
+        
+    // }
 }))
 
 app.listen(port, () => console.log('[+] Server listening on port %d', port))
